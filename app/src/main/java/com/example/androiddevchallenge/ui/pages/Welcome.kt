12065092679553504444505a0e3be12245f20e50
and2long <and2long@gmail.com>
2021-03-13 22:51:41 +0800

    格式化代码。

--- app/src/main/java/com/example/androiddevchallenge/ui/pages/Welcome.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/pages/Welcome.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.pages
 
 import androidx.compose.foundation.Image
@@ -54,12 +69,14 @@ fun Welcome() {
                     end.linkTo(parent.end)
                 }
             )
-            Box(modifier = Modifier
-                .height(32.dp)
-                .fillMaxWidth()
-                .constrainAs(descText) {
-                    top.linkTo(logoText.bottom)
-                }) {
+            Box(
+                modifier = Modifier
+                    .height(32.dp)
+                    .fillMaxWidth()
+                    .constrainAs(descText) {
+                        top.linkTo(logoText.bottom)
+                    }
+            ) {
                 Text(
                     text = "Beautiful home garden solutions",
                     style = MaterialTheme.typography.subtitle1.copy(color = MaterialTheme.colors.onPrimary),
@@ -83,11 +100,13 @@ fun Welcome() {
                     style = MaterialTheme.typography.button.copy(color = if (darkTheme) gray else white),
                 )
             }
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(btnLogin) {
-                    top.linkTo(btnRegister.bottom, margin = 20.dp)
-                }) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(btnLogin) {
+                        top.linkTo(btnRegister.bottom, margin = 20.dp)
+                    }
+            ) {
                 Text(
                     text = "Log in",
                     style = MaterialTheme.typography.button.copy(color = if (darkTheme) white else pink900),
@@ -97,7 +116,6 @@ fun Welcome() {
         }
     }
 }
-
 
 @Preview(widthDp = 360)
 @Composable
